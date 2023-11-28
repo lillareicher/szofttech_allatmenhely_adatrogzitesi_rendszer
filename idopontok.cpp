ea3898a5ad1eb3b9f80f5059ee0096e181b31158
BensPan <151777322+BensPan@users.noreply.github.com>
2023-11-28 06:45:32 +0100

    Add files via upload

--- idopontok.cpp
+++ idopontok.cpp
@@ -6,3 +6,33 @@ Idopontok::Idopontok(int _ev, int _honap, int _nap, int _ora) :
     nap(_nap),
     ora(_ora)
 {}
+
+bool Idopontok::validEv(int ev)
+{
+    if(ev>2022 && ev<2031) return true;
+    return false;
+}
+
+bool Idopontok::validHonap(int honap)
+{
+    if(honap<13 && honap > 0) return true;
+    return false;
+}
+
+bool Idopontok::validNap(int nap)
+{
+    if(nap>0 && nap<32) return true;
+    return false;
+}
+
+bool Idopontok::validOra(int ora)
+{
+    if(ora >= 0 && ora < 25) return true;
+    return false;
+}
+
+bool Idopontok::validDatum(int ev, int honap, int nap, int ora)
+{
+    if(validEv(ev) == true && validHonap(honap)==true && validNap(nap)==true && validOra(ora)==true) return true;
+    return false;
+}
--- idopontok.h
+++ idopontok.h
@@ -9,4 +9,9 @@ public:
     int nap;
     int ora;
     Idopontok(int _ev, int _honap, int _nap, int _ora);
+    static bool validEv(int ev);
+    static bool validHonap(int honap);
+    static bool validNap(int nap);
+    static bool validOra(int ora);
+    static bool validDatum(int ev, int honap, int nap, int ora);
 };
